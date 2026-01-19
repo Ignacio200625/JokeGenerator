4e5c8e7a03151148c1b2b209b226af3bb219b38f
Ignacio <alumno.770209@ies-azarquiel.es>
2026-01-19 22:30:44 +0100

    secondCommit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  root: "./src", // Cambia la raíz del proyecto a "src"
+  build: {
+    outDir: "../docs", // Los archivos construidos irán a la carpeta "docs"
+  },
 })
